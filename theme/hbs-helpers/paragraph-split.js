e9f4a7cddf254ffde77f28d65dd7362ad63ff844
Geoffroy Stolaric <geoffroy.stolaric@maximo.fr>
2024-02-21 11:52:27 +0100

    split para correction <p> + Ajout schema.org + clean

--- theme/hbs-helpers/paragraph-split.js
+++ theme/hbs-helpers/paragraph-split.js
@@ -4,12 +4,17 @@ const markdown = require('markdown-it')();
 const paragraphSplit = (text) => {
   const expr = /\r\n|\r|\n/g;
 
+
   if (text != null) {
     text = markdown.render(text);
   }
 
   const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr);
-  const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
+
+  let output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
+  output = output.replace("<p><p>", '<p>');
+  output = output.replace("</p></p>", '</p>');
+
   return new SafeString(output);
 };
 
